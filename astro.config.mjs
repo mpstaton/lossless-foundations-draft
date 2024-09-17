26d8aa226040eadeceec2a9a71088f5e8ea5e9e4
Michael Staton <mpstaton@gmail.com>
2024-09-17 10:27:01 -0500

    trying to load a content module to see if that works

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import content from '@astrojs/content';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        resolve: {
+          preserveSymlinks: true
+        }
+      },
+      integrations: [content()],
+});
