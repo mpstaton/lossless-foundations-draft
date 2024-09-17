c2bb44e4af90d8164f33343188aa5c7e86c7f513
Michael Staton <mpstaton@gmail.com>
2024-09-17 02:01:00 -0500

    been trying to get markdown to load, started the learnastro.dev course from teachable

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection } from 'astro:content';
+import { z } from 'zod';
+// 2. Define your collection(s)
+const sectionsCollection = defineCollection({ 
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.date(),
+        recentUpdate: z.date(),
+        author: z.string(),
+        tags: z.array(z.string()),
+    }),
+});
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  'sections': sectionsCollection,
+};
---
+++ src/content/sections/introduction.md
@@ -0,0 +1,12 @@
+---
+layout: ../../layouts/ContentPageLayout.astro
+title: "Introduction"
+pubDate: 2024-09-16
+recentUpdate: 2024-09-16
+author: "Michael Staton"
+tags: ["introduction", "about"]
+---
+
+#Introduction
+
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis ullam iste sequi excepturi explicabo animi sit doloribus nobis quasi? Nemo sunt facilis delectus molestiae aspernatur, porro est debitis commodi voluptatem.
